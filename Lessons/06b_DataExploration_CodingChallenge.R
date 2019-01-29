2e329eeca5b7f1e7202e665fc6d164567252cee8
Sebastian Bognar <Seabass@Sebastians-MacBook-Air.local>
2019-01-29 13:03:21 -0500

    working on challenge 6

--- Lessons/06b_DataExploration_CodingChallenge.R
+++ Lessons/06b_DataExploration_CodingChallenge.R
@@ -5,27 +5,28 @@
 ## Instructions listed in comments throughout the script.
 
 #### Setup ----
-setwd("/Users/katerisalk/Documents/Duke/Courses/Environmental_Data_Analytics")
+getwd()
+setwd("/Users/Seabass/Documents/Duke/spring_2019/env_872L/lesson_2/ENV_872L")
 
 library("tidyverse")
 
-ecotox.neonic <- read.csv("/Data/Raw/ECOTOX_Neonicotinoids_Mortality_raw.csv")
+ecotox.neonic <- read.csv("./Data/Raw/ECOTOX_Neonicotinoids_Mortality_raw.csv")
 
 #### Basic Data Summaries ----
-head(ecotox.neonic,)
-summary(ecotox.neonic%Chemical.Name)
-summary(ecotox.neonic%Pub..Year)
+head(ecotox.neonic)
+summary(ecotox.neonic$Chemical.Name)
+summary(ecotox.neonic$Pub..Year)
 
 # Fix formatting of column names (spaces originally present were turned to periods upon import)
-colnames(ecotox.neonic)[8:12] <- c(Duration, Conc.Type, Conc.Mean, Conc.Units, Pub.Year)
+colnames(ecotox.neonic)[8:12] <- c("Duration", "Conc.Type", "Conc.Mean", "Conc.Units", "Pub.Year")
 
 # Plot histogram of counts of publication years
-ggplot(ecotox.neonic, aes(x = Pub.Year)) 
+ggplot(ecotox.neonic, aes(x = Pub.Year)) +
   geom_histogram()
 
 # Plot histogram of counts chemical names
 # hint: what is the class of Chemical.Name? There are two options for a solution.
-ggplot(ecotox.neonic, x = Chemical.Name) +
+ggplot(ecotox.neonic, aes( = Chemical.Name)) +
   geom_histogram()
 
 # Plot frequency polygon of publication years divided by chemical name
